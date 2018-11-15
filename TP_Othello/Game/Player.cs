eae0df78910a255086a1109c5b4e2728874ac3ee
Ludovic Herbelin <ludovic.herbelin@gmail.com>
2018-11-15 19:04:44 +0100

    Base folder with IPlayable interface and Player class

---
+++ TP_Othello/Game/Player.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP_Othello.Game
+{
+    class Player : System.Runtime.Serialization.ISerializable
+    {
+        Stack<Object> lastPlays;
+
+        public Player()
+        {
+            lastPlays = new Stack<object>();
+        }
+
+        public void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
